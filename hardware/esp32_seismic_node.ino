092bade5f5ed1bd1442049bec2de8a7045293cf4
JITENDRARAUT <jitendraraut903@gmail.com>
2026-07-08 23:16:17 +0545

    esp32_seismic_node.ino

--- hardware/esp32_seismic_node.ino
+++ hardware/esp32_seismic_node.ino
@@ -4,7 +4,7 @@
 #include <Wire.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-
+ 
 const char* WIFI_SSID = "YOUR_WIFI_NETWORK";
 const char* WIFI_PASS = "YOUR_WIFI_PASSWORD";
 const char* MQTT_SERVER = "broker.hivemq.com";
